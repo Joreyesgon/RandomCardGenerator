2a7e0ccefee5390d1b323b3a8e426052e4e06fc0
Joresgon <jo.reyesgon@gmail.com>
2021-08-18 17:41:19 -0400

    First commit

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 window.onload = function () {
 window.genCard = function () {
 
-    var suit = ["♦","♥","♠","♣"];
-    var numbers = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+    let suit = ["♦","♥","♠","♣"];
+    let numbers = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
 
         function randomCard(){
-            var randomSuit = suit[Math.floor(Math.random() * suit.length)];
-            var randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+            let randomSuit = suit[Math.floor(Math.random() * suit.length)];
+            let randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
 
             document.querySelector(".iconTop").innerHTML = randomSuit;
             document.querySelector(".cardNumber").innerHTML = randomNumber;
             document.querySelector(".iconBottom").innerHTML = randomSuit;
 
        
-            if (randomSuit == "♦" || randomSuit == "♥"){
+            if (randomSuit === "♦" || randomSuit === "♥"){
                 document.querySelector("#cardContainer").style.color = "red";
             }   
 
-            else if (randomSuit == "♠" || randomSuit == "♣"){
+            else if (randomSuit === "♠" || randomSuit === "♣"){
                 document.querySelector("#cardContainer").style.color = "black"
             }
         }
