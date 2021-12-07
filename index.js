4bca473ed271043592c23699f1287195fe966215
Joresgon <jo.reyesgon@gmail.com>
2021-12-07 19:49:21 -0300

    minor change

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 window.onload = function () {
 window.genCard = function () {
 
-    let suit = ["♦","♥","♠","♣"];
+    let suits = ["♦","♥","♠","♣"];
     let numbers = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
 
         function randomCard(){
-            let randomSuit = suit[Math.floor(Math.random() * suit.length)];
+            let randomSuit = suits[Math.floor(Math.random() * suits.length)];
             let randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
 
             document.querySelector(".iconTop").innerHTML = randomSuit;
